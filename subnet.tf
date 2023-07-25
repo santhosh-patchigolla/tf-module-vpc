cb977d9305cbd969d7c84a6a66fb826e16e1271d
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-25 16:46:39 +0530

    sdc

--- subnet.tf
+++ subnet.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "public_subnet" {
 
   vpc_id                    = aws_vpc.main.id
   cidr_block                = element(var.PUBLIC_SUBNET_CIDR, count.index)
-
+  availability_zone         = element(var.AZ, count.index)
   tags = {
     Name                    = "roboshop-${var.ENV}-public-subnet-${element(var.AZ, count.index)}"
   }
@@ -15,7 +15,7 @@ resource "aws_subnet" "private_subnet" {
 
   vpc_id                    = aws_vpc.main.id
   cidr_block                = element(var.PRIVATE_SUBNET_CIDR, count.index)
-
+  availability_zone         = element(var.AZ, count.index)
   tags = {
     Name                    = "roboshop-${var.ENV}-private-subnet-${element(var.AZ, count.index)}"
   }
