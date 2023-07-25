d7bd16cf144e691dd19a304bb7ebd07b5ac4802b
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-25 16:35:28 +0530

    sdc

--- subnet.tf
+++ subnet.tf
@@ -10,6 +10,17 @@ resource "aws_subnet" "public_subnet" {
 }
 
 
+resource "aws_subnet" "private_subnet" {
+  count                     = length(var.PRIVATE_SUBNET_CIDR)
+
+  vpc_id                    = aws_vpc.main.id
+  cidr_block                = element(var.PRIVATE_SUBNET_CIDR, count.index)
+
+  tags = {
+    Name                    = "roboshop-${var.ENV}-private-subnet-${element(var.AZ, count.index)}"
+  }
+}
+
 
 
 
