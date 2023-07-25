bc60102b97120d9d0891c09bc923c98b98212bc7
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-25 17:32:52 +0530

    sdc

--- route-table.tf
+++ route-table.tf
@@ -12,7 +12,7 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-# Below will be adding the subnet asscociatate to the public-RT
+# Below will be adding the subnet asscociatate to the publicSubnet-RT
 resource "aws_route_table_association" "public_subnet_rt_association" {
   count                 = length(aws_subnet.public_subnet.*.id)
 
@@ -21,15 +21,23 @@ resource "aws_route_table_association" "public_subnet_rt_association" {
 }
  
 # Below creates Private Route Table
-/* resource "aws_route_table" "public_rt" {
+resource "aws_route_table" "private_rt" {
   vpc_id                = aws_vpc.main.id
-
+/* 
   route {
     cidr_block          = "0.0.0.0/0"
     gateway_id          = aws_internet_gateway.igw.id
-  }
+  } */
 
   tags = {
-    Name = "roboshop-${var.ENV}-public-rt"
+    Name = "roboshop-${var.ENV}-private-rt"
   }
-} */
+}
+
+# Below will be adding the subnet asscociatate to the privateSubnet-RT
+resource "aws_route_table_association" "private_subnet_rt_association" {
+  count                 = length(aws_subnet.private_subnet.*.id)
+
+  subnet_id             = element(aws_subnet.private_subnet.*.id, count.index)
+  route_table_id        = aws_route_table.private_rt.id
+}
