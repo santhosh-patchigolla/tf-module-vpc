3f6a75093f5948112f47c9ab3762aac919f4ba7c
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-25 11:08:09 +0530

    sdc

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,9 @@
-resource "aws_vpc" "main" {
-  cidr_block        =   var.VPC_CIDR
+  resource "aws_vpc" "main" {
+  cidr_block            =   var.VPC_CIDR
+
+  tags = {
+    Name = "roboshop-${var.ENV}-vpc"
+  }  
 }
+
+
