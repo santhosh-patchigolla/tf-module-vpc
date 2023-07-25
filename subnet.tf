36f07f019747646b575980d4a990057168004f7b
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-25 16:50:06 +0530

    sdc

--- subnet.tf
+++ subnet.tf
@@ -4,6 +4,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                    = aws_vpc.main.id
   cidr_block                = element(var.PUBLIC_SUBNET_CIDR, count.index)
   availability_zone         = element(var.AZ, count.index)
+  map_public_ip_on_launch   = true
   tags = {
     Name                    = "roboshop-${var.ENV}-public-subnet-${element(var.AZ, count.index)}"
   }
