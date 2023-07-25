7fca9b22b8ecd21e6e3b239126df370610287891
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-25 17:08:24 +0530

    sdc

---
+++ igw.tf
@@ -0,0 +1,18 @@
+# creates the internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "roboshop-${var.ENV}-igw"
+  }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ route-table.tf
@@ -0,0 +1,27 @@
+# Below creates Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id                = aws_vpc.main.id
+
+  route {
+    cidr_block          = "0.0.0.0/0"
+    gateway_id          = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public-rt"
+  }
+}
+
+# Below creates Private Route Table
+/* resource "aws_route_table" "public_rt" {
+  vpc_id                = aws_vpc.main.id
+
+  route {
+    cidr_block          = "0.0.0.0/0"
+    gateway_id          = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public-rt"
+  }
+} */
