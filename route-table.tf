aa6f951d32f76f5fd6e3de082289e2ec68df82c4
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-25 17:22:33 +0530

    sdc

--- route-table.tf
+++ route-table.tf
@@ -12,6 +12,14 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+# Below will be adding the subnet asscociatate to the public-RT
+resource "aws_route_table_association" "public_subnet_rt_association" {
+  count                 = length(aws_subnet.public_subnet.*.id)
+
+  subnet_id             = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id        = aws_route_table.public_rt.id
+}
+ 
 # Below creates Private Route Table
 /* resource "aws_route_table" "public_rt" {
   vpc_id                = aws_vpc.main.id
