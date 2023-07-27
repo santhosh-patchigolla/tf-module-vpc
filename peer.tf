7d611c2b0ebe2b768632b84008a1e6dbef9caf71
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-27 12:42:12 +0530

    sdc

--- peer.tf
+++ peer.tf
@@ -1,10 +1,12 @@
-# using bewlow we creating the VPC peering and it will auto connect()auto accept as we doing it in the same account
-resource "aws_vpc_peering_connection_accepter" "peer" {
-  vpc_id                    = aws_vpc.main.id
-  vpc_peering_connection_id =var.DEFAULT_VPC_ID
-  auto_accept               = true
+/* # Requester's side of the connection.
+resource "aws_vpc_peering_connection" "peer" {
+  peer_vpc_id   = aws_vpc.main.id
+  vpc_id        = var.DEFAULT_VPC_ID 
+  auto_accept   = true
 
   tags = {
     Side = "Robot-${var.ENV}-default-vpc-peering"
   }
-}
+} */
+
+
