d517f683d2045a817c77e5c00ae0d4b99a6b4c41
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-27 13:54:25 +0530

    sdc

--- vars.tf
+++ vars.tf
@@ -5,6 +5,7 @@ variable "PUBLIC_SUBNET_CIDR" {}
 variable "PRIVATE_SUBNET_CIDR" {}
 variable "DEFAULT_VPC_ID" {}
 variable "DEFAULT_VPC_CIDR" {}
+variable "DEFAULT_VPC_RT" {}
 
 
 # added the values
